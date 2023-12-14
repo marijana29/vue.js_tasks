e8166b0dc5c24811bf51ce27cc8aa6c02407bdd6
marijana29 <marijana.kalicanin@yahoo.com>
2023-12-14 21:20:24 +0100

    solved the task with user list

---
+++ Task_App/vue_task/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <UserList />
+  </div>
+</template>
+
+<script>
+import UserList from "./components/UserList.vue";
+
+export default {
+  name: "App",
+  components: {
+    UserList,
+  },
+};
+</script>
---
+++ Task_App/vue_task/src/components/UserListDetail.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="user-list__details">
+    <h2 class="user-list__details-hl">Details</h2>
+    <template v-if="user">
+      <dl>
+        <dt>name:</dt>
+        <dd>{{ user.name }}</dd>
+
+        <dt>email:</dt>
+        <dd>{{ user.email }}</dd>
+
+        <dt>phone:</dt>
+        <dd>{{ user.phone }}</dd>
+
+        <dt>company:</dt>
+        <dd>{{ user.company.name }}</dd>
+      </dl>
+    </template>
+    <template v-else>
+      <p class="user-list__details-hint">
+        Es wurde kein Nutzer gefunden / ausgewählt
+      </p>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: Object,
+  },
+  created() {
+    console.log("Received User in UserListDetail:", this.user);
+  },
+};
+</script>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+.user-list__details {
+  width: 60%;
+  padding: 1rem;
+  color: #192a56;
+}
+.user-list__details-hl {
+  color: #00a8ff;
+}
+.user-list__details dd {
+  margin-bottom: 1rem;
+}
+.user-list__details-hint {
+  border: 2px solid #e84118;
+  border-radius: 5px;
+  color: #e84118;
+  padding: 0.5rem;
+}
+</style>
